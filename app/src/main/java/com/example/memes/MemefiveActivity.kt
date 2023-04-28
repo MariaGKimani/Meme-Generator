f2c8c62d74b8325439ec574bbda96ef6e14a5290
Mariagoretti <mariakimanigoreti@gmail.com>
2023-04-28 05:57:26 +0300

    added a meme

--- app/src/main/java/com/example/memes/MemefiveActivity.kt
+++ app/src/main/java/com/example/memes/MemefiveActivity.kt
@@ -7,21 +7,15 @@ import android.widget.Button
 
 class MemefiveActivity : AppCompatActivity() {
     lateinit var btn11: Button
-    lateinit var btn12: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memefive)
         btn11 = findViewById(R.id.btn11)
-        btn12 = findViewById(R.id.btn12)
         btn11.setOnClickListener {
             val intent = Intent(this,MemefourActivity::class.java)
             startActivity(intent)
         }
-        btn12.setOnClickListener {
-            val intent = Intent(this, MemesixActivity::class.java)
-            startActivity(intent)
 
-        }
 
     }
 }
